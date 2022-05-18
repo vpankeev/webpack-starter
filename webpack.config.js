be0d7df818a841165c09d50b4e2c4a6ff0b6af37
Vlad <v.pankeev95@gmail.com>
2022-05-18 13:21:18 +0300

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
     context: path.resolve(__dirname, "src"),
     entry: {
-        app: ['babel-polyfill', './js/app.js']
+        app: ['./js/app.js']
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
